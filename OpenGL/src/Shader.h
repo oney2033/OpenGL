4d09b3654f97eac8e092f68fae2ae04c2c12cc60
oney2033 <131510065+oney2033@users.noreply.github.com>
2024-07-05 19:05:39 +0800

    设置一个2D纹理，以及导入stb_image库

--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -21,7 +21,10 @@ public:
 	void Bind()const;
 	void Unbind()const;
 
+	void SetUniform1i(const std::string& name, int value);
+	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+
 
 	ShaderProgramSource ParseShader(const std::string& filepath);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
