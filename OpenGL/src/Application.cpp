1233a1d53e9261181775fa9789c45fc69f93360b
oney2033 <131510065+oney2033@users.noreply.github.com>
2024-07-06 11:28:53 +0800

    导入Imgui 图形用户界面库

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -13,6 +13,8 @@
 #include "Texture.h"
 #include"glm/glm.hpp"
 #include"glm/gtc/matrix_transform.hpp"
+#include"imgui/imgui.h"
+#include"imgui/imgui_impl_glfw_gl3.h"
 
 int main(void)
 {
@@ -74,18 +76,16 @@ int main(void)
 
         IndexBuffer ib(indices, 6);
 
+        /* 模型视图投影矩阵 */
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
         glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
-
-        glm::mat4 mvp = proj * view * model; /* 模型视图投影矩阵 */
         
         /* 从文件中解析着色器源码 */
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
 
-        shader.SetUniformMat4f("u_MVP", mvp);
+       
 
         /*加载纹理*/
         Texture texture("res/textures/ChernoLogo.png");
@@ -99,6 +99,17 @@ int main(void)
         ib.Unbind();
 
         Renderer renderer;
+
+        ImGui::CreateContext();
+        ImGui_ImplGlfwGL3_Init(window, true);
+        ImGui::StyleColorsDark();
+
+        glm::vec3 translation(200, 0, 0);
+
+        bool show_demo_window = true;
+        bool show_another_window = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
         float r = 0.0f;
         float increment = 0.05f;
 
@@ -108,8 +119,15 @@ int main(void)
             /* Render here */
             renderer.Clear();
 
+            ImGui_ImplGlfwGL3_NewFrame();
+
+             glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+            glm::mat4 mvp = proj * view * model; /* 模型视图投影矩阵 */
+
             shader.Bind();
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+
+            shader.SetUniformMat4f("u_MVP", mvp);
 
             va.Bind();
             ib.Bind();
@@ -126,6 +144,14 @@ int main(void)
             }
             r += increment;
 
+            {
+                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f); 
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            }
+
+            ImGui::Render();
+            ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
@@ -133,6 +159,9 @@ int main(void)
             glfwPollEvents();
         }
     }
+
+    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui::DestroyContext();
     glfwTerminate();
     return 0;
 }
