7509fddb94330ac5c5432cdc7571bb217c636981
oney2033 <131510065+oney2033@users.noreply.github.com>
2024-07-06 13:34:54 +0800

    渲染两个图像

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -48,10 +48,10 @@ int main(void)
         /* 顶点位置浮点型数组 */
         float positions[] = 
         {
-            100.0f, 100.0f, 0.0f, 0.0f, // 0
-            200.0f, 100.0f, 1.0f, 0.0f,  // 1
-            200.0f, 200.0f, 1.0f, 1.0f,    // 2
-            100.0f, 200.0f, 0.0f, 1.0f   // 3
+             -50.0f, -50.0f, 0.0f, 0.0f, // 0
+              50.0f, -50.0f, 1.0f, 0.0f, // 1
+              50.0f, 50.0f, 1.0f, 1.0f,  // 2
+             -50.0f, 50.0f, 0.0f, 1.0f   // 3
         };
 
         /* 索引缓冲区所需索引数组 */
@@ -78,7 +78,7 @@ int main(void)
 
         /* 模型视图投影矩阵 */
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
         
         /* 从文件中解析着色器源码 */
         Shader shader("res/shaders/Basic.shader");
@@ -104,7 +104,8 @@ int main(void)
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
 
-        glm::vec3 translation(200, 0, 0);
+        glm::vec3 tranlationA(200, 200, 0);
+        glm::vec3 tranlationB(400, 200, 0);
 
         bool show_demo_window = true;
         bool show_another_window = false;
@@ -121,18 +122,25 @@ int main(void)
 
             ImGui_ImplGlfwGL3_NewFrame();
 
-             glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-            glm::mat4 mvp = proj * view * model; /* 模型视图投影矩阵 */
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), tranlationA);
+                glm::mat4 mvp = proj * view * model;
 
-            shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
+                shader.Bind();
+                shader.SetUniformMat4f("u_MVP", mvp);
 
-            shader.SetUniformMat4f("u_MVP", mvp);
+                renderer.Draw(va, ib, shader);
+            }
 
-            va.Bind();
-            ib.Bind();
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), tranlationB);
+                glm::mat4 mvp = proj * view * model;
 
-            renderer.Draw(va,ib,shader); // 绘制
+                shader.Bind();
+                shader.SetUniformMat4f("u_MVP", mvp);
+
+                renderer.Draw(va, ib, shader);
+            }
 
             if (r > 1.0f)
             {
@@ -145,7 +153,8 @@ int main(void)
             r += increment;
 
             {
-                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f); 
+                ImGui::SliderFloat3("Translation", &tranlationA.x, 0.0f, 960.0f); 
+                ImGui::SliderFloat3("TranlationB", &tranlationB.x, 0.0f, 960.0f);
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             }
 
